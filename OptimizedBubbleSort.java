7c58d0f7f47d25f57395d8f215e5186fc0cc2ded
ShakyaSRA <shakyaroo2006@gmail.com>
2026-01-13 13:17:19 +0530

    Create OptimizedBubbleSort.java

---
+++ OptimizedBubbleSort.java
@@ -0,0 +1,48 @@
+package data.structures;
+
+public class OptimizedBubbleSort {
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap adjacent elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no swap happened, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Display array
+    static void printArray(int[] arr) {
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        System.out.print("Before Sorting: ");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.print("After Sorting: ");
+        printArray(arr);
+    }
+}
