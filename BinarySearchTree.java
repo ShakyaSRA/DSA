489705f1af47e933872f00acac006d3992270367
ShakyaSRA <shakyaroo2006@gmail.com>
2026-01-27 09:21:14 +0530

    Create BinarySearchTree.java

---
+++ BinarySearchTree.java
@@ -0,0 +1,71 @@
+package data.structures;
+
+public class BinarySearchTree {
+
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    // Insert a node
+    Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (data < root.data)
+            root.left = insert(root.left, data);
+        else if (data > root.data)
+            root.right = insert(root.right, data);
+
+        return root;
+    }
+
+    // Search an element
+    boolean search(Node root, int key) {
+        if (root == null)
+            return false;
+
+        if (root.data == key)
+            return true;
+
+        if (key < root.data)
+            return search(root.left, key);
+
+        return search(root.right, key);
+    }
+
+    // Inorder Traversal (Sorted order)
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.root = bst.insert(bst.root, 50);
+        bst.insert(bst.root, 30);
+        bst.insert(bst.root, 70);
+        bst.insert(bst.root, 20);
+        bst.insert(bst.root, 40);
+        bst.insert(bst.root, 60);
+        bst.insert(bst.root, 80);
+
+        System.out.print("Inorder Traversal: ");
+        bst.inorder(bst.root);
+
+        System.out.println("\nSearch 40: " + bst.search(bst.root, 40));
+        System.out.println("Search 90: " + bst.search(bst.root, 90));
+    }
+}
